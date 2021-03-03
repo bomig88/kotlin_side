73469898c92e78d51c27e787b4b1db1e4af03fc7
권보미 <bomi_g@emforce.co.kr>
2021-03-03 17:25:51 +0900

    -Firebase Auth 연동 (Google Type) -registerForActivityResult 학습 중

--- app/src/main/java/bomi/kotlinside/ui/home/HomeFragment.kt
+++ app/src/main/java/bomi/kotlinside/ui/home/HomeFragment.kt
@@ -1,11 +1,21 @@
 package bomi.kotlinside.ui.home
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import bomi.kotlinside.R
 import bomi.kotlinside.databinding.FragmentHomeBinding
 import bomi.kotlinside.base.ui.BaseFragment
+import bomi.kotlinside.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.lang.StringBuilder
 
@@ -36,6 +46,39 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
             viewDataBinding.tvResult.text = builder.toString()
         }
+
+        FirebaseAuth.getInstance().currentUser?.let {
+            Log.d("TAGGGGG", "already login // " + it.email)
+            Toast.makeText(mContext, "login!! " + it.email, Toast.LENGTH_SHORT).show()
+
+        } ?: registerForActivityResult(object : ActivityResultContract<ArrayList<AuthUI.IdpConfig>, FirebaseUser?>() {
+            override fun createIntent(context: Context, input: ArrayList<AuthUI.IdpConfig>): Intent {
+                return AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(input)
+                    .build()
+            }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): FirebaseUser? {
+                return when (resultCode) {
+                    Activity.RESULT_OK -> FirebaseAuth.getInstance().currentUser
+                    else -> null
+                }
+            }
+
+        }) {
+            it?.let {
+                Log.d("TAGGGGG", "login success // " + it.email)
+                Toast.makeText(mContext, "login!! " + it.email, Toast.LENGTH_SHORT).show()
+            }
+        }.launch(arrayListOf(
+//            AuthUI.IdpConfig.EmailBuilder().build(),
+//            AuthUI.IdpConfig.PhoneBuilder().build(),
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+//            AuthUI.IdpConfig.TwitterBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        )
     }
 
     override fun refresh() { }
